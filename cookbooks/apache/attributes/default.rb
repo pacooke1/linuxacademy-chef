378ca1619247db049ffc6a1a849bc4a15e9e1f5c
pacooke1 <philip.a.cooke1@gmail.com>
2017-01-19 11:46:52 +0000

    Commit chef-repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+default["apache"]["sites"]["pacooke12"] = {"port" => 80, "domain" => "pacooke12.mylabserver.com" }
+default["apache"]["sites"]["pacooke12b"] = {"port" => 80, "domain" => "pacooke12b.mylabserver.com" }
+
